4661cc89294c8a8733f310b39bcecc6b052d3d9b
Bhavin karena <bhavinkarena2003@gmail.com>
2024-04-09 16:24:04 +0530

    .

--- src/app.js
+++ src/app.js
@@ -7,23 +7,38 @@ import { facebookPassport, googlePassport } from "./controllers/user.controller.
 
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.send("D8teme")
+})
+
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
+
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
+
+  // app.use((req, res, next) => {
+  //   // Set CORS headers
+  //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow requests from any origin
+  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specified HTTP methods
+  //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specified headers
+  //   next();
+  // });
+
 app.use(
-  session({
-    secret: 'bhavinkarena',
-    resave: false,
-    saveUninitialized: false,
-  })
+    session({
+      secret: 'bhavinkarena',
+      resave: false,
+      saveUninitialized: false,
+    })
 );
 
-app.use(express.static("public"));
-app.use(cookieParser());
+app.use(express.static("public"))   
+app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
@@ -34,22 +49,22 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //user routes import
-import UserRouter from './routes/user.route.js';
-app.use("/api/v1", UserRouter);
+import UserRouter from './routes/user.route.js'
+app.use("/api/v1", UserRouter)
 //userProfile routes import
-import userprofileRouter from "./routes/userprofile.route.js";
-app.use("/api/v2",userprofileRouter);
+import userprofileRouter from "./routes/userprofile.route.js"
+app.use("/api/v2",userprofileRouter)
 //hashtag route import
-import hashRouter from "./routes/hashtag.route.js";
-app.use("/api/v3",hashRouter);
+import hashRouter from "./routes/hashtag.route.js"
+app.use("/api/v3",hashRouter)
 //request route import
-import requestRouter from "./routes/usermatch.route.js";
-app.use("/api/v4",requestRouter);
+import requestRouter from "./routes/usermatch.route.js"
+app.use("/api/v4",requestRouter)
 //post route import
-import postRouter from "./routes/post.route.js";
-app.use("/api/v5",postRouter);
+import postRouter from "./routes/post.route.js"
+app.use("/api/v5",postRouter)
 //comment route import
-import commentRouter from "./routes/comment.route.js";
-app.use("/api/v6",commentRouter);
+import commentRouter from "./routes/comment.route.js"
+app.use("/api/v6",commentRouter)
 
 export { app };
