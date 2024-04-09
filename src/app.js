21ccb3e32aca4af66880581d9d89bad81b4fe40d
Bhavin karena <bhavinkarena2003@gmail.com>
2024-04-09 15:53:38 +0530

    e

--- src/app.js
+++ src/app.js
@@ -12,11 +12,13 @@ app.get("/",(req,res)=>{
 })
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: "http://localhost:3000",
     credentials: true
   }));
+
   app.use((req, res, next) => {
     // Set CORS headers
+    console.log("req.headers");
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow requests from any origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specified HTTP methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specified headers
