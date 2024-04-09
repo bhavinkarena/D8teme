7bfc4907f0a2a314b22129679bba58c767d3bc89
Bhavin karena <bhavinkarena2003@gmail.com>
2024-04-09 18:04:42 +0530

    ecerc

--- src/app.js
+++ src/app.js
@@ -34,9 +34,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://d8teme-752t.onrender.com/","https://d8tome.vercel.app/"],
+  origin: ["http://localhost:3000", "https://d8teme-752t.onrender.com/","https://d8tome.vercel.app/","https://d8tome.vercel.app"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
+  preflightContinue: false,
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
