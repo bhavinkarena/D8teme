be81d2501654532e9474d6d01e9f50974d502b4f
Bhavin karena <bhavinkarena2003@gmail.com>
2024-04-10 18:37:47 +0530

    .

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,6 +8,7 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
+    console.log("localFilePath", localFilePath);
     //upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
