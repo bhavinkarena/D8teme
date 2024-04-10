2d6e8a127421907fb5c51e7d69e4cd7b27f3888e
Bhavin karena <bhavinkarena2003@gmail.com>
2024-04-10 10:39:29 +0530

    abhi

--- src/app.js
+++ src/app.js
@@ -33,12 +33,21 @@ facebookPassport(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors({
-  origin: ["http://localhost:3000", "https://d8teme-752t.onrender.com", "https://d8tome.vercel.app"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://d8teme-752t.onrender.com",
+      "https://d8tome.vercel.app",
+      "https://d8teme-752t.onrender.com/api/v1/google",
+      "https://d8teme-752t.onrender.com/api/v1/google/callback",
+      "https://d8teme-752t.onrender.com/",
+    ],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 //user routes import
 import UserRouter from "./routes/user.route.js";
