5a7368fd75a2e87d8517a40b574794ef88e010f2
Bhavin karena <bhavinkarena2003@gmail.com>
2024-04-10 12:04:27 +0530

    .

--- src/app.js
+++ src/app.js
@@ -8,29 +8,8 @@ import {
   googlePassport,
 } from "./controllers/user.controller.js";
 
-// Import routes
-import UserRouter from "./routes/user.route.js";
-import userprofileRouter from "./routes/userprofile.route.js";
-import hashRouter from "./routes/hashtag.route.js";
-import requestRouter from "./routes/usermatch.route.js";
-import postRouter from "./routes/post.route.js";
-import commentRouter from "./routes/comment.route.js";
-
 const app = express();
 
-// Middleware
-app.use(express.static("public"));
-app.use(cors({
-  origin: "*", // Allow requests from any origin
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}));
-app.use(cookieParser());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-// Session middleware
 app.use(
   session({
     secret: "bhavinkarena",
@@ -39,27 +18,54 @@ app.use(
   })
 );
 
-// Initialize Passport and configure strategies
-app.use(passport.initialize());
-app.use(passport.session());
-googlePassport(passport);
-facebookPassport(passport);
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-// Routes
 app.get("/", (req, res) => {
   res.send("D8teme");
 });
-app.use("/api/v1", UserRouter);
-app.use("/api/v2", userprofileRouter);
-app.use("/api/v3", hashRouter);
-app.use("/api/v4", requestRouter);
-app.use("/api/v5", postRouter);
-app.use("/api/v6", commentRouter);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-});
+googlePassport(passport);
+facebookPassport(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://d8teme-752t.onrender.com",
+      "https://d8tome.vercel.app",
+      "https://d8teme-752t.onrender.com/api/v1/google",
+      "https://d8teme-752t.onrender.com/api/v1/google/callback",
+      "https://d8teme-752t.onrender.com/",
+    ],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+//user routes import
+import UserRouter from "./routes/user.route.js";
+app.use("/api/v1", UserRouter);
+//userProfile routes import
+import userprofileRouter from "./routes/userprofile.route.js";
+app.use("/api/v2", userprofileRouter);
+//hashtag route import
+import hashRouter from "./routes/hashtag.route.js";
+app.use("/api/v3", hashRouter);
+//request route import
+import requestRouter from "./routes/usermatch.route.js";
+app.use("/api/v4", requestRouter);
+//post route import
+import postRouter from "./routes/post.route.js";
+app.use("/api/v5", postRouter);
+//comment route import
+import commentRouter from "./routes/comment.route.js";
+app.use("/api/v6", commentRouter);
 
 export { app };
